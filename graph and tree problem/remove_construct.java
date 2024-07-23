141143225fbb1108ebb42d5493282b4fb3e416d1
Mohanraj S <147261507+mohanraj1203@users.noreply.github.com>
2024-07-23 10:37:56 +0530

    Update remove_construct.java

--- graph and tree problem/remove_construct.java
+++ graph and tree problem/remove_construct.java
@@ -1,3 +1,18 @@
 import java.util.*;
 class remove_construct{
-	
+	public static void main(String[] arg){
+		Scanner obj=new Scanner(System.in);
+		String str=obj.nextLine().toLowerCase();
+		String temp="juspay";
+		int i=0,j=0;
+		while(i < temp.length() && j < str.length()){
+			if(temp.charAt(i) == str.charAt(j))
+				i++;
+			j++;
+		}
+		if(i == temp.length())
+			System.out.println(str.length() - temp.length());
+		else
+			System.out.println(-1);
+	}
+}
